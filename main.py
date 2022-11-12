e94b23ac8cdc51d0d1664000dbd188cd6b584b72
Sanya Zaicev <sanyazaicev325@gmail.com>
2022-11-12 13:26:42 +0300

    добавил отрицание пустых строк

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ class CoolAccount:
         self.login = login
         self.password = password
         self.diamonds = diamonds
+        self.create_user()
 
     def create_user(self):
         if self.login == "" or self.password =="":
@@ -35,14 +36,14 @@ def sign_up():
 
     sign_up_screen = Toplevel(main)
     sign_up_screen.title("РЕГИСТРАЦИЯ!")
-    sign_up_screen.geometry("300x200")
+    sign_up_screen.geometry("400x200")
 
     #Labels
     Label(sign_up_screen, text="Введите логин и пароль").grid(row=0, sticky=N, pady=10) #row  строка
     Label(sign_up_screen, text="Как тебя звать?").grid(row=1, sticky=W) #row  строка
     Label(sign_up_screen, text="А какой пароль?").grid(row=2, sticky=W) #row  строка
     notif = Label(sign_up_screen, font=("Calibri", 12))
-    notif.grid(row=3, sticky=N, pady=10)
+    notif.grid(row=4, sticky=N, pady=10)
     # Entries
     Entry(sign_up_screen, textvariable=s_up_login).grid(row=1, column=1)
     Entry(sign_up_screen, textvariable=s_up_password).grid(row=2, column=1)
