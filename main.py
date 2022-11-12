146ddf9c3101b09a8f7d56761235e30264af3f6c
Sanya Zaicev <sanyazaicev325@gmail.com>
2022-11-12 13:03:56 +0300

    самая лучшая игра 2.0

--- main.py
+++ main.py
@@ -7,16 +7,48 @@ main = Tk()
 main.title("mycraft")
 main.geometry("300x300") # размер
 
+class CoolAccount:
+    def __init__(self, login, password, diamonds=0):
+        self.login = login
+        self.password = password
+        self.diamonds = diamonds
+
+    def create_user(self):
+        if self.login == "" or self.password =="":
+            notif.config(text="Необходимо заполнить все поля", fg="red")
+
+
+
+def create_account():
+    login = s_up_login.get()
+    password = s_up_password.get()
+    CoolAccount(login, password)
+
+
 #reg
 def sign_up():
+    global s_up_login
+    global s_up_password
+    global notif
+    s_up_login = StringVar()
+    s_up_password = StringVar()
+
     sign_up_screen = Toplevel(main)
     sign_up_screen.title("РЕГИСТРАЦИЯ!")
-    sign_up_screen.geometry("200x200")
+    sign_up_screen.geometry("300x200")
 
     #Labels
     Label(sign_up_screen, text="Введите логин и пароль").grid(row=0, sticky=N, pady=10) #row  строка
     Label(sign_up_screen, text="Как тебя звать?").grid(row=1, sticky=W) #row  строка
     Label(sign_up_screen, text="А какой пароль?").grid(row=2, sticky=W) #row  строка
+    notif = Label(sign_up_screen, font=("Calibri", 12))
+    notif.grid(row=3, sticky=N, pady=10)
+    # Entries
+    Entry(sign_up_screen, textvariable=s_up_login).grid(row=1, column=1)
+    Entry(sign_up_screen, textvariable=s_up_password).grid(row=2, column=1)
+
+    # Buttons
+    Button(sign_up_screen, text="РЕГИСТРАЦИЯ", font=("Calibri", 12), width=15, command=create_account).grid(row=3)
 
 
 #картинка
